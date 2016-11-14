c9364a71b329883b110e6e4b1b6b88615b75c9de
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-14 16:08:30 +0000

    Add packages/lttng-babeltrace.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/lttng-babeltrace.sh
@@ -0,0 +1,17 @@
+check_pkg_lttng_babeltrace() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://www.efficios.com/files/babeltrace/ | grep -o -E "babeltrace-[0-9]+\.[0-9]+\.[0-9]+\.tar\.(gz|xz|bz2)" | tail -n1 | grep -o -E "[0-9]+\.[0-9]+\.[0-9]+")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
