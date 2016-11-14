057b9bfd44db95f7dbb8ac5bcf76deb2815f73b9
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-14 16:08:30 +0000

    Add packages/nmap.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/nmap.sh
@@ -0,0 +1,17 @@
+check_pkg_nmap() {
+	local br_version=$(grep -E "^${1^^}_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://nmap.org/dist/ | grep -o -E "nmap-[0-9]+\.[0-9]+\.tar.(gz|xz|bz2)" | tail -n1 | grep -o -E "[0-9]+\.[0-9]+")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
