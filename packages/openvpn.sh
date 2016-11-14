3e53c9533c3465596eb0a14c5f31bfaaa5a880b3
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-14 16:08:30 +0000

    Add packages/openvpn.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/openvpn.sh
@@ -0,0 +1,17 @@
+check_pkg_openvpn() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://openvpn.net/index.php/open-source/downloads.html | grep -o -E "OpenVPN [0-9]+\.[0-9]+\.[0-9]+" | head -n1 | grep -o -E "[0-9]+\.[0-9]+\.[0-9]+")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
