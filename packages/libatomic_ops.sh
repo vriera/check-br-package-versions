0d81f538c58e096118d1ea73e49b3f5946d178db
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-23 14:48:16 +0000

    Add packages/libatomic_ops.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/libatomic_ops.sh
@@ -0,0 +1,17 @@
+check_pkg_libatomic_ops() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://github.com/ivmai/libatomic_ops/releases | grep -o -E "libatomic_ops-[0-9]+_[0-9]+([a-z]|_[0-9]+)?" | head -n1)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
