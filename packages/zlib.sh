02fe264b9003530aab01733d6cd8ad1015d21aea
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-01-16 14:49:08 +0000

    Add packages/zlib.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/zlib.sh
@@ -0,0 +1,17 @@
+check_pkg_zlib() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://www.${1}.net/ | grep "Current release:" -A 3 | grep -o -E "[0-9]+\.[0-9]+(\.[0-9]+)?")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
