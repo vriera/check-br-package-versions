bb2c473a115845300cfb05842ecf4f414ec0e906
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-01-17 10:45:17 +0000

    Add packages/mono.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/mono.sh
@@ -0,0 +1,17 @@
+check_pkg_mono() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://www.mono-project.com/download/ | grep "The latest Mono release is" | grep -o -E "[0-9]+\.[0-9]+(\.[0-9]+)*" | tail -1)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
