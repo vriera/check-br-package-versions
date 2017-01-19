a3b33d09aab2669573a8d3a9347f9e33421f1c0a
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-01-19 11:29:47 +0000

    Add packages/pulseaudio.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/pulseaudio.sh
@@ -0,0 +1,17 @@
+check_pkg_pulseaudio() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://www.freedesktop.org/wiki/Software/PulseAudio/ | grep "has been released" | head -1 | grep -o -E "[0-9]+\.[0-9]+(\.[0-9]+)*" | head -1)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
