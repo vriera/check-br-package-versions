26038d7e9a0a435db9d900ba913ab9d4c5d89148
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-03-14 14:26:31 +0000

    Add packages/check.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/check.sh
@@ -0,0 +1,17 @@
+check_pkg_check() {
+	local br_version=$(grep -E "^${1^^}_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://github.com/libcheck/${1}/releases |grep "Latest release" -A 5 | grep -o -E "([0-9]+\.){2}[0-9]+" | tail -1)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
