e05b51247a080db5c5c88b1dfc8f9dfeabd1a7e9
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-03-14 14:44:13 +0000

    Add packages/libscrypt.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/libscrypt.sh
@@ -0,0 +1,17 @@
+check_pkg_libscrypt() {
+	local br_version=$(grep -E "^${1^^}_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://github.com/technion/${1}/releases | grep -o -E "tag/v[0-9]+\.[0-9]+" | head -1 | sed 's:^tag/::')
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
