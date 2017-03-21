5a97bf2ab04d702ee4ee848588bd2285c84c5802
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-03-21 16:11:14 +0000

    Add packages/firejail.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/firejail.sh
@@ -0,0 +1,17 @@
+check_pkg_firejail() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://firejail.wordpress.com/ | grep -o -E 'released Firejail [0-9]+(\.[0-9]+)*' | head -1 | grep -o -E '[0-9]+(\.[0-9]+)*')
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
