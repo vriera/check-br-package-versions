3f9b47794c9ad68c02b18acd6febbd3c7bfa5069
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-03-24 13:18:03 +0000

    Add packages/python.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/python.sh
@@ -0,0 +1,19 @@
+check_pkg_python() {
+	local br_version_major=$(grep -E "^${1^^}_VERSION_MAJOR = " package/${1}/${1}.mk | awk '{print $3}')
+	local br_version_minor=$(grep -E "^${1^^}_VERSION = " package/${1}/${1}.mk | cut -d'.' -f2-)
+	local br_version="${br_version_major}.${br_version_minor}"
+	local version=$(wget -q -O - https://www.python.org/ | grep "/downloads/release/python-2" | grep -o -E "2\.[0-9]+(\.[0-9]+)*")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
