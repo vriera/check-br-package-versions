e0a28e924f248f74ff8a38c1e498562abb2166c2
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-03-24 14:11:40 +0000

    Add packages/wiringpi.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/wiringpi.sh
@@ -0,0 +1,17 @@
+check_pkg_wiringpi() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - "https://git.drogon.net/?p=wiringPi;a=tags" | grep "commit" | grep -o -E "[0-9]+\.[0-9]+" | head -1)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
