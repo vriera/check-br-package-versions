9fd37125bf444cdadb43127dbbe32c7b02163a16
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-03-29 14:56:50 +0100

    Add packages/ntfs-3g.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/ntfs-3g.sh
@@ -0,0 +1,17 @@
+check_pkg_ntfs_3g() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://www.tuxera.com/community/open-source-ntfs-3g/ | grep 'stable version' | head -1 | grep -o -E '[0-9]{4}\.1?[0-9]\.[0-3]?[0-9]' | tail -1)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
