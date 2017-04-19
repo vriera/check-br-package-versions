1f652bfcbb02ece98473bcd1d454a69189bbf00a
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-04-19 15:20:29 +0100

    Add packages/libdrm.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/libdrm.sh
@@ -0,0 +1,17 @@
+check_pkg_libdrm() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://dri.freedesktop.org/${1}/ | grep -o -E "${1}-[0-9]+(\.[0-9]+)*" | sort -V | tail -1 | cut -d'-' -f2)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
