3116e073760d7490c093c3d79c216ce9aba66ed4
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-10 15:12:17 +0100

    Add packages/swig.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/swig.sh
@@ -0,0 +1,19 @@
+check_pkg_swig() {
+	local br_version_major=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION_MAJOR = " package/${1}/${1}.mk | awk '{print $3}')
+	local br_version_minor=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | cut -d'.' -f2-)
+	local br_version="${br_version_major}.${br_version_minor}"
+	local version=$(wget -q -O - http://www.${1}.org/ | grep -o -E "SWIG-[0-9]+(\.[0-9]+)* released" | head -1 | grep -o -E "[0-9]+(\.[0-9]+)*")
+
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
