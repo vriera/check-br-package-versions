247393491e12749cb9866ed9a073fed3e975a6c3
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-12 10:25:10 +0100

    Add packages/vlc.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/vlc.sh
@@ -0,0 +1,17 @@
+check_pkg_vlc() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://www.videolan.org/vlc/download-sources.html | grep "Latest VLC source code tarball" | head -1 | grep -o -E "[0-9]+(\.[0-9]+)*")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
