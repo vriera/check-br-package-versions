1322f6288d191b2ee778dae8a68be36055047ffb
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-26 11:58:27 +0100

    Add packages/sngrep.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/sngrep.sh
@@ -0,0 +1,17 @@
+check_pkg_sngrep() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://github.com/irontec/${1}/releases |grep "Latest release" -A 5 | grep -o -E "v[0-9]+(\.[0-9]+)*" | tail -n1)
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
