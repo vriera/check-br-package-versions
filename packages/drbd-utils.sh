c6061322b4b821c1a50b43bb99f32e3b3ec7a456
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-26 11:25:49 +0100

    Add packages/drbd-utils.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/drbd-utils.sh
@@ -0,0 +1,17 @@
+check_pkg_drbd_utils() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://oss.linbit.com/drbd/ | grep -o -E "drbd-utils-[0-9]+(\.[0-9]+)*" | sort -V | tail -1 | sed 's/^drbd-utils-//')
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
