8adaa22306087122dafa5f2508abd90c3be72986
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-30 11:13:05 +0100

    Add packages/emlog.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/emlog.sh
@@ -0,0 +1,17 @@
+check_pkg_emlog() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://github.com/nicupavel/${1}/releases | grep -o -E "${1}/releases/tag/emlog-[0-9]+(\.[0-9]+)*" | head -n1 | awk -F '/' '{print $NF}')
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
