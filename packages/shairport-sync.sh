e5a4918124e2650615369acd045300288abe332c
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-30 13:06:28 +0100

    Add packages/shairport-sync.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/shairport-sync.sh
@@ -0,0 +1,24 @@
+check_pkg_shairport_sync() {
+	local github_user="mikebrady"
+	local github_project="$1"
+	local remove_v=true
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://github.com/${github_user}/${github_project}/releases/latest | grep -o -E "/${github_user}/${github_project}/releases/tag/([^\"])*" | head -1 | awk -F "/" '{print $NF}')
+	
+	if $remove_v; then
+		version=$(echo $version | sed 's/^v//')
+	fi
+
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
