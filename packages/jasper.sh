a5e03eddb17b3780a6be7d10c4284f33ec7e2537
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-06-02 16:18:05 +0100

    packages/jasper.sh: fix version retrieval
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

--- packages/jasper.sh
+++ packages/jasper.sh
@@ -1,7 +1,14 @@
 check_pkg_jasper() {
+	local github_user="mdadams"
+	local github_project="$1"
+	local remove_v=false
 	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
-	local version=$(wget -q -O - http://www.ece.uvic.ca/~frodo/jasper/#download | grep -o -E "jasper-[0-9]+\.[0-9]+\.[0-9]+\.tar\.(gz|xz|bz2)" | head -n1 | grep -o -E "[0-9]+\.[0-9]+\.[0-9]+")
+	local version=$(wget -q -O - https://github.com/${github_user}/${github_project}/releases | grep -o -E "/${github_user}/${github_project}/releases/tag/([^\"])*" | head -1 | awk -F "/" '{print $NF}')
 	
+	if $remove_v; then
+		version=$(echo $version | sed 's/^v//')
+	fi
+
 	if [[ "$br_version" != "$version" ]]; then
 		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
 			packages="$packages $1"
