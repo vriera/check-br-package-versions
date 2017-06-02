c8e31a00845e50b9421573ecf4552092e7437a1b
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-06-02 16:16:13 +0100

    Add packages/openldap.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/openldap.sh
@@ -0,0 +1,17 @@
+check_pkg_openldap() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://www.openldap.org/ | grep -o -E "OpenLDAP [0-9]+(\.[0-9]+)*" | sed 's/OpenLDAP //')
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
