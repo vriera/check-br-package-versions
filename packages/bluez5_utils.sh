b362091f0f8bba379ad6cd50e7f9aaf91ef36fdb
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-06-02 12:55:12 +0100

    Add packages/bluez5_utils.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/bluez5_utils.sh
@@ -0,0 +1,17 @@
+check_pkg_bluez5_utils() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - https://git.kernel.org/pub/scm/bluetooth/bluez.git/refs/tags | grep -o -E "bluez-5(\.[0-9]+)*\.tar" | sort -V | tail -1 | grep -o -E "5(\.[0-9]+)*")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
