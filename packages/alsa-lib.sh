e4c3441b4dece0c80a7fa55472cd9190d48f39a7
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-06-02 16:29:33 +0100

    Add packages/alsa-lib.sh
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>

---
+++ packages/alsa-lib.sh
@@ -0,0 +1,17 @@
+check_pkg_alsa_lib() {
+	local br_version=$(grep -E "^$(echo ${1^^} | sed 's/-/_/g')_VERSION = " package/${1}/${1}.mk | awk '{print $3}')
+	local version=$(wget -q -O - http://www.alsa-project.org/main/index.php/Main_Page  | grep -o -E "alsa-lib-[0-9]+(\.[0-9]+)*" | sort -V | tail -1 | grep -o -E "[0-9]+(\.[0-9]+)*")
+	
+	if [[ "$br_version" != "$version" ]]; then
+		if [[ "$br_version" != "" ]] && [[ "$version" != "" ]]; then
+			packages="$packages $1"
+			br_versions="$br_versions $br_version"
+			versions="$versions $version"
+		else
+			echo "Warning: $1 code has a problem."
+		fi
+	fi
+
+	unset br_version
+	unset version
+}
